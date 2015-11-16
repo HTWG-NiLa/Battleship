780ad434a5df348f97f7f7b666b445e3384b6a56
Lars Knaack <lars.knaack@transdocu.de>
2015-11-16 15:40:37 +0100

    Added first files

---
+++ src/main/java/model/impl/Battleground.java
@@ -0,0 +1,11 @@
+package main.java.model.impl;
+
+import main.java.model.IBattleground;
+
+public class Battleground implements IBattleground {
+
+	public void addShip(Ship ship, Field destination, int orientation) {
+		
+	}
+
+}
---
+++ src/main/java/model/impl/Ship.java
@@ -0,0 +1,31 @@
+package main.java.model.impl;
+
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.model.IShip;
+
+public class Ship implements IShip {
+	
+	private List<Field> fields = new ArrayList<Field>();
+	private Rectangle2D shape;
+
+	public boolean isDestroyed() {
+		for(Field each : fields) {
+			if (!each.isBombed()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void create(List<Field> destination) {
+		shape = new Rectangle2D.Double();
+		for(Field each : destination) {
+			each.setShip(this);
+			shape.add(each.getArea());
+		}
+		fields = new ArrayList<Field>(destination);
+	}
+}
